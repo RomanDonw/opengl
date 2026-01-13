fb44937d4e6447424a2966b33cf4ea679dc9c827
RomanDonw <romandonw628@gmail.com>
2026-01-13 16:17:20 +1000

    Revert "setupped Bullet Physics"
    This reverts commit c5bfeffc1b787263bc69d423001987e6b4cc5a4f.

--- build.bat
+++ build.bat
@@ -5,13 +5,10 @@ set LIB=.\lib
 set SRC=.\src
 
 echo Building GLAD...
-rem gcc -shared -I%INCLUDE% %SRC%\lib\glad.c -o %LIB%\glad.dll -Wl,--out-implib,%LIB%\libglad.dll.a
-gcc -c -I%INCLUDE% %SRC%\lib\glad.c -o %LIB%\glad.o
-ar rcs %LIB%\libglad.a %LIB%\glad.o
-del %LIB%\glad.o
+gcc -shared -I%INCLUDE% %SRC%\lib\glad.c -o %LIB%\glad.dll -Wl,--out-implib,%LIB%\libglad.dll.a
 
 echo Building main...
-g++ -static -I%INCLUDE% -I%INCLUDE%\bullet -L%LIB% %SRC%\main.cpp -o .\main.exe -lglad -lglfw3 -lopengl32 -lgdi32 -lLinearMath -lBulletDynamics -lBulletCollision
+g++ -I%INCLUDE% -L%LIB% %SRC%\main.cpp -o .\main.exe -lglad -lglfw3 -lopengl32 -lgdi32
 
-rem copy %LIB%\glfw3.dll .
-rem copy %LIB%\glad.dll .
+copy %LIB%\glfw3.dll .
+copy %LIB%\glad.dll .
