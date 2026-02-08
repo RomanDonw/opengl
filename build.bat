aef6b78f8e72c6585ee7d19ec9eb3751ee1bdda2
RomanDonw <romandonw628@gmail.com>
2026-02-08 12:15:06 +1000

    added C/C++ std libs static linking

--- build.bat
+++ build.bat
@@ -6,11 +6,11 @@ set SRC=.\src
 
 echo Building main...
 : g++ -static -I%INCLUDE% -L%LIB% %SRC%\main.cpp -o .\main.exe -lglad -lglfw3 -lopengl32 -lgdi32
-g++ -c -I%INCLUDE% -L%LIB% %SRC%\main.cpp -o main.o
+g++ -c -I%INCLUDE% -L%LIB% %SRC%\main.cpp -o main.o -static-libgcc -static-libstdc++
 
 if not exist .\main.o goto on_error
     echo Linking main...
-    g++ -I%INCLUDE% -L%LIB% ./main.o %LIB%\libglad.a %LIB%\libglfw3.a -lopengl32 -lgdi32 -lsoft_oal -lOpenAL32 -o ./main.exe
+    g++ -I%INCLUDE% -L%LIB% -static-libgcc -static-libstdc++ ./main.o %LIB%\libglad.a %LIB%\libglfw3.a -lopengl32 -lgdi32 -lsoft_oal -lOpenAL32 -o ./main.exe
     del main.o
 
     : copy %LIB%\glfw3.dll .
