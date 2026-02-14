f13254ed305c1bf9a2ba3dcd946af5a9e01771f3
RomanDonw <126128023+RomanDonw@users.noreply.github.com>
2026-02-14 22:59:19 +1000

    changed comment type in build_glad.bat (blank commit)

--- build_glad.bat
+++ build_glad.bat
@@ -5,7 +5,7 @@ set LIB=.\lib
 set SRC=.\lib\src
 
 echo Building GLAD...
-rem gcc -shared -I%INCLUDE% %SRC%\lib\glad.c -o %LIB%\glad.dll -Wl,--out-implib,%LIB%\libglad.dll.a
+: gcc -shared -I%INCLUDE% %SRC%\lib\glad.c -o %LIB%\glad.dll -Wl,--out-implib,%LIB%\libglad.dll.a
 gcc -c -I%INCLUDE% %SRC%\glad.c -o %LIB%\glad.o
 ar rcs %LIB%\libglad.a %LIB%\glad.o
 del %LIB%\glad.o
